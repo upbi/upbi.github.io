704f470682a9af364356f001f6138d0d54edc43b
abdrb <abdrb22@gmail.com>
2025-10-10 11:43:35 +0300

    update index.html

---
+++ js/links.js
@@ -0,0 +1,22 @@
+var website = ""; var link = ""; var social="";
+var loca = window.location.pathname;
+var wpath = loca.substring(0, loca.lastIndexOf("/"));
+var dName = wpath.substring(wpath.lastIndexOf(".")+1);
+if(dName!=="io"){var website = "../";}
+function funclinks(){
+link += "<li><a href='" + website + "index.html'>Home</a></li>";
+
+link += "<li><a href='" + website + "reqactive.html'>Activation Code</a></li>";
+
+document.getElementById("links").innerHTML=link;
+
+social += "<a href='https://codepen.io/'><i class='fa fa-codepen'></i></a>";
+social += "<a href='https://www.linkedin.com/in/'><i class='fa fa-linkedin-square'></i></a>";
+social += "<a href='https://www.facebook.com/'><i class='fa fa-facebook-square'></i></a>";
+social += "<a href=''><i class='fa fa-twitter'></i></a>";
+social += "<a href='https://www.youtube.com/'><i class='fa fa-youtube'></i></a>";
+social += "<a href=''><i class='fa fa-whatsapp'></i></a>";
+social += "<a href='https://www.instagram.com/'><i class='fa fa-instagram'></i></a>";
+social += "<a href='feedback.html'><i class='fa fa-envelope'></i></a>";
+document.getElementById("sociallinks").innerHTML=social;
+}
