bf526be88219342cf5906fa584b945dc36f5abdf
upbi <abdrb22@yahoo.com>
2025-10-10 19:51:26 +0300

    Translate to Arabic

--- js/links.js
+++ js/links.js
@@ -4,19 +4,19 @@ var wpath = loca.substring(0, loca.lastIndexOf("/"));
 var dName = wpath.substring(wpath.lastIndexOf(".")+1);
 if(dName!=="io"){var website = "../";}
 function funclinks(){
-link += "<li><a href='" + website + "index.html'>Home</a></li>";
+link += "<li><a href='" + website + "index.html'>الصفحة الرئيسية</a></li>";
 
-link += "<li><a href='" + website + "reqactive.html'>Activation Code</a></li>";
+link += "<li><a href='" + website + "reqactive.html'>طلب كود التفعيل</a></li>";
 
 document.getElementById("links").innerHTML=link;
 
-social += "<a href='https://codepen.io/'><i class='fa fa-codepen'></i></a>";
-social += "<a href='https://www.linkedin.com/in/'><i class='fa fa-linkedin-square'></i></a>";
-social += "<a href='https://www.facebook.com/'><i class='fa fa-facebook-square'></i></a>";
-social += "<a href=''><i class='fa fa-twitter'></i></a>";
-social += "<a href='https://www.youtube.com/'><i class='fa fa-youtube'></i></a>";
-social += "<a href=''><i class='fa fa-whatsapp'></i></a>";
-social += "<a href='https://www.instagram.com/'><i class='fa fa-instagram'></i></a>";
-social += "<a href='feedback.html'><i class='fa fa-envelope'></i></a>";
+//social += "<a href='https://codepen.io/'><i class='fa fa-codepen'></i></a>";
+//social += "<a href='https://www.linkedin.com/in/'><i class='fa fa-linkedin-square'></i></a>";
+//social += "<a href='https://www.facebook.com/'><i class='fa fa-facebook-square'></i></a>";
+//social += "<a href=''><i class='fa fa-twitter'></i></a>";
+//social += "<a href='https://www.youtube.com/'><i class='fa fa-youtube'></i></a>";
+//social += "<a href=''><i class='fa fa-whatsapp'></i></a>";
+//social += "<a href='https://www.instagram.com/'><i class='fa fa-instagram'></i></a>";
+social += "<a href='reqactive.html'><i class='fa fa-envelope'></i></a>";
 document.getElementById("sociallinks").innerHTML=social;
 }
